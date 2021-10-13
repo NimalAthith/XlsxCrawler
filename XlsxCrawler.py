3eec82178ebe456fae32dffafe8acd7c0b0ef71c
NimalAthith <tnimalathith@gmail.com>
2021-10-13 07:36:45 +0530

    Added file crawler

--- XlsxCrawler.py
+++ XlsxCrawler.py
@@ -1,12 +1,26 @@
 ##reading existing xlsx file
 
 from openpyxl import load_workbook
+#for handling file
+import os
+
+
+path = './source'
+
+def openFile(keyword):
+#save files as list
+    files = os.listdir(path)
+    for file in files:
+        if file.startswith(keyword):
+            print (file)
+
 
 wb = load_workbook(filename="Bom_Compare.xlsx")
 
 ws = wb.active
 part_name = ws['C']
 
-# pritn the content
+# print the content
 for x in range(len(part_name)):
     print(part_name[x].value)
+    openFile(part_name[x].value)
